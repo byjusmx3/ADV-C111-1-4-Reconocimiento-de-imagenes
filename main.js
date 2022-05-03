267124169a783fda6b614f2f929db511679b661e
byjusmx3 <90639391+byjusmx3@users.noreply.github.com>
2022-05-03 11:17:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function gotResult(error, results) {
   if (error) {
     console.error(error);
   } else {
-    // Los resultados están en una matriz ordenada por confianza.
+    // Los resultados están en una matriz ordenados por la confianza.
     console.log(results);
     document.getElementById("result_object_name").innerHTML = results[0].label;
     document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
